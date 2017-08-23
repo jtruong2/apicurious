66ec81b7e4decfbeec3f54773fa9b91ec4999163
jtruong2 <jtruong2@comcast.net>
2017-08-22 22:37:28 -0600

    adds vcr gem/ config

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'awesome_print'
   gem 'launchy'
+  gem 'vcr'
+  gem 'webmock'
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,13 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'webmock/rspec'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/cassettes"
+  config.hook_into :webmock
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 def stub_omniauth
   OmniAuth.config.test_mode = true
