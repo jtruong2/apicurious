6cedf455a9f921e1840e0e8149db7d3ec8605ed4
jtruong2 <jtruong2@comcast.net>
2017-08-23 20:38:08 -0600

    adds organizations

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
   def index
+    @organizations = find_organizations(current_user.token, current_user.username)
   end
 end
--- spec/features/user_sees_organizations_spec.rb
+++ spec/features/user_sees_organizations_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 RSpec.describe "User visits dashboard" do
   it "sees a list of affiliated organizations" do
+    stub_omniauth
 
+    visit root_path
+    click_on "Sign in with Github"
+    expect(current_path).to eq(dashboard_path)
+
+    expect(page).to have_content("Organizations")
+    expect(page).to have_content("")
   end
 end
